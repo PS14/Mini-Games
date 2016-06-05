52a888e5eb6a213ca6b0220807574c15fb722bb5
PS14 <phantasystar2115@yahoo.co.jp>
2016-06-05 19:32:14 +0900

    メニュー追加

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,14 @@
 #include"Resource\Object\stdafx.h"
 #include"Resource\Scene\Title\Title.h"
 #include"Resource\Scene\Menu\Menu.h"
+#include"Resource\Scene\Game_Menu\BattingGame_Menu.h"
+#include"Resource\Scene\Game_Menu\InvaderGame_Menu.h"
+#include"Resource\Scene\Game_Menu\PuzzleGame_Menu.h"
+#include"Resource\Scene\Game_Menu\ShootingGame_Menu.h"
+#include"Resource\Scene\Game_Menu\TargetGame_Menu.h"
 #include "Resource\Scene\Game\PuzzleGame.h"
-#include"Resource\Scene\Game\Shootinggame.h"
-#include"Resource\Scene\Game\baseballgame.h"
+#include"Resource\Scene\Game\ShootingGame.h"
+#include"Resource\Scene\Game\BattingGame.h"
 #include"Resource\Scene\Game\InvaderGame.h"
 #include"Resource\Scene\Game\TargetGame.h"
 #include"Resource\Scene\Result\Result.h"
@@ -14,13 +19,26 @@ void Main()
 	MyApp manager;
 
 	// シーンを設定
+	
+	//タイトル
 	manager.add<Title>(L"Title");
+	//メインメニュー
 	manager.add<Menu>(L"Menu");
-	manager.add<PuzzleGame>(L"PuzzleGame");
-	manager.add<Shootinggame>(L"Shootinggame");
-	manager.add<BaseBall>(L"BaseBall");
-	manager.add<Invader>(L"Invader");
-	manager.add<TargetGame>(L"TargetGame");
+	//各メニュー
+	manager.add<BattingGame_Menu>(L"BattingGame_Menu");
+	manager.add<PuzzleGame_Menu>(L"PuzzleGame_Menu");
+	manager.add<ShootingGame_Menu>(L"ShootingGame_Menu");
+	manager.add<InvaderGame_Menu>(L"InvaderGame_Menu");
+	manager.add<TargetGame_Menu>(L"TargetGame_Menu");
+
+	//ゲーム
+	manager.add<Puzzle_Game>(L"Puzzle_Game");
+	manager.add<Shooting_Game>(L"Shooting_Game");
+	manager.add<Batting_Game>(L"Batting_Game");
+	manager.add<Invader_Game>(L"Invader_Game");
+	manager.add<Target_Game>(L"Target_Game");
+	
+	//リザルト
 	manager.add<Result>(L"Result");
 
 	while (System::Update())
